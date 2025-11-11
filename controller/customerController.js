0ddf33f1b6f64821eab236c1b5561420f8fc5394
saumy <saumyadivyanjalee529@gmail.com>
2025-11-11 13:57:49 +0530

    completed model

--- controller/customerController.js
+++ controller/customerController.js
@@ -1,3 +1,18 @@
-import CustomerModel from "../model/customerModel";
-import {customers_array} from "../db/database";
-import {setDataDrop}
+import CustomerModel from "../model/customerModel.js";
+import {customers_array} from "../db/database.js";
+import {setDataDropdowns} from "./orderController.js";
+import {loadAllOrders,setTotalValues} from "./dashboardController.js";
+import {NAME,NIC,EMAIL,TEL,QTY} from "../util/regex";
+import {setAlert} from "../util/alert.js";
+
+let cusBody = $("#customer-tbl-body");
+let cusId = $("#inputId");
+let cusName = $("#inputName");
+let cusAddress = $("#inputAddress");
+let cusNic = $("#inputNic");
+let cusEmail = $("#inputEmail")
+let cusTel = $("#inputTel");
+
+//save
+
+
--- model/customerModel.js
+++ model/customerModel.js
@@ -1,42 +1,59 @@
-export default class CustomerModel {
-    constructor(id, name, address,nic,email,phone) {
-        this._customer_id  = id;
+export default class CustomerModel{
+    constructor(id,name,address,nic,email,tel){
+        this._customer_id = id;
         this._name = name;
         this._address = address;
+        this._nic = nic;
         this._email = email;
-        this._phone = phone;
-
+        this._tel = tel;
     }
+
 
     get customer_id() {
         return this._customer_id;
     }
+
     set customer_id(value) {
         this._customer_id = value;
     }
+
     get name() {
         return this._name;
     }
+
     set name(value) {
         this._name = value;
     }
+
     get address() {
         return this._address;
     }
-    set address(value){
+
+    set address(value) {
         this._address = value;
     }
+
+    get nic() {
+        return this._nic;
+    }
+
+    set nic(value) {
+        this._nic = value;
+    }
+
     get email() {
         return this._email;
     }
+
     set email(value) {
         this._email = value;
     }
-    get phone() {
-        return this._phone;
 
+    get tel() {
+        return this._tel;
     }
-    set phone(value) {
-        this._phone = value;
+
+    set tel(value) {
+        this._tel = value;
     }
 }
